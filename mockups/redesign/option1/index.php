699e3e5ddbda5b737a497e26a8b5421b36842f7f
Nathaniel Bockoven <nbockoven@gmail.com>
2015-06-10 15:49:03 -0700

    add more redesign options & change file permissions

---
+++ mockups/redesign/option1/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Nathaniel Bockoven | Web Developer | Portfolio</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.4/paper/bootstrap.min.css">
+    <link rel="stylesheet" href="css/core-layout.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>
+          Nathaniel Bockoven
+          <br>
+          <small>web developer</small>
+        </h1>
+      </div><!-- .container -->
+    </div><!-- .jumbotron -->
+
+    <div class="container">
+
+      <div class="row">
+        <div class="col-sm-4">
+          <img src="#" alt="me" class="img-responsive">
+        </div><!-- .col-sm-4 -->
+        <div class="col-sm-4">
+          <img src="#" alt="me" class="img-responsive">
+        </div><!-- .col-sm-4 -->
+        <div class="col-sm-4">
+          <img src="#" alt="me" class="img-responsive">
+        </div><!-- .col-sm-4 -->
+      </div><!-- .row -->
+
+
+      <div class="row">
+        <div class="col-sm-9">
+          <h2 class="mt-0">web</h2>
+        </div><!-- .col-sm-9 -->
+        <div class="col-sm-3">
+          <img src="#" alt="picture of website" class="responsive">
+        </div><!-- .col-sm-3 -->
+      </div><!-- .row -->
+
+
+      <div class="row">
+        <div class="col-sm-3">
+          <img src="#" alt="picture of website" class="responsive">
+        </div><!-- .col-sm-3 -->
+        <div class="col-sm-9">
+          <h2 class="mt-0">skillset</h2>
+        </div><!-- .col-sm-9 -->
+      </div><!-- .row -->
+
+
+      <div class="row">
+        <div class="col-sm-9">
+          <h2 class="mt-0">about me</h2>
+        </div><!-- .col-sm-9 -->
+        <div class="col-sm-3">
+          <img src="#" alt="me" class="responsive">
+        </div><!-- .col-sm-3 -->
+      </div><!-- .row -->
+
+    </div><!-- .container -->
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  </body>
+</html>
