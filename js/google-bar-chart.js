8af6f49fbff3e37f03a958944821395f54bcdba1
Nathaniel Bockoven <nbockoven@gmail.com>
2015-12-11 15:59:59 -0800

    sync

--- js/google-bar-chart.js
+++ js/google-bar-chart.js
@@ -1,39 +1,89 @@
-google.load("visualization", "1", {packages:["corechart"]});
+google.load("visualization", "1.1", {packages:["bar"]});
 
-google.setOnLoadCallback( drawChart );
+var CHART_HAS_CHANGED = 0;
+var chartID = 'chart-bar-skillset';
 
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ["Element", "Density", { role: "style" } ],
+var data = [];
 
-    ["PHP", 78.94, "#006699"],
-    ["MySQL", 60.49, "#006699"],
-    ["JavaScript", 20.30, "#006699"],
-    ["jQuery", 30.45, "color: #006699"],
-    ["AngularJS", 10.45, "color: #006699"],
-  ]);
+var options = {
+  animation: {
+    duration: 10000,
+    startup: true
+  },
+  backgroundColor: 'transparent',
+  bars: 'horizontal', // Required for Material Bar Charts.
+  colors: ['#006699'],
+  hAxis: {
+    // format: 'percent'
+  },
+};
 
-  var view = new google.visualization.DataView(data);
-  view.setColumns(
-    [
-      0,
-      1,
-      {
-        calc: "stringify",
-        sourceColumn: 1,
-        type: "string",
-        role: "annotation"
-      },
-      2
-    ]
-  );
+var skillset = [
+  [ // on load
+    ['Skill',      'Percent'],
+    ['PHP',        0],
+    ['MySQL',      0],
+    ['JavaScript', 0],
+    ['jQuery',     0]
+  ],
+  [ // on change
+    ['Skill',      'Percent'],
+    ['PHP',        80],
+    ['MySQL',      85],
+    ['JavaScript', 60],
+    ['jQuery',     70]
+  ]
+];
 
-  var options = {
-    backgroundColor: 'transparent',
-    legend: { position: "none" },
-  };
+var chart;
 
-  var chart = new google.visualization.BarChart(document.getElementById("chart-bar-skillset"));
+function drawChart( iteration ){
+  var deferred = $.Deferred();
+  iteration = ( parseInt( iteration ) ) ? 1 : 0;
+  console.log( iteration );
+  google.visualization.events.addListener(chart, 'ready', function(){});
+  // chart.draw(data, options);
+  chart.draw(data[iteration], google.charts.Bar.convertOptions( options ));
+  return deferred.resolve().promise();
+}
 
-  chart.draw( view, options );
+function initGoogleChart(){
+  var deferred = $.Deferred();
+  chart = new google.charts.Bar(document.getElementById( chartID ));
+  $.each(skillset, function( i, v ){
+    data[i] = google.visualization.arrayToDataTable( v );
+  });
+  return deferred.resolve().promise();
+}
+
+
+$(document).ready(function(){
+
+  $.when( initGoogleChart() )
+   .then( drawChart() )
+   .then( checkIfChartIsSeen() );
+});
+$(window).scroll( checkIfChartIsSeen );
+
+
+function checkIfChartIsSeen(){
+  if( !CHART_HAS_CHANGED ){
+    // get chart div
+    var chart = $('#'+chartID);
+    // chart lives between...
+    var chartTop    = chart.offset().top;
+    var chartBottom = chartTop + chart.innerHeight;
+    // window is showing...
+    var windowTop    = window.pageYOffset;
+    var windowBottom = windowTop + window.innerHeight;
+
+    if(
+      (chartTop > windowTop && chartTop < windowBottom) ||
+      (chartBottom > windowTop && chartBottom < windowBottom)
+    ){
+      // change the chart values to trigger chart animation
+      CHART_HAS_CHANGED = 1;
+      drawChart( 1 );
+    }
+  }
 }
