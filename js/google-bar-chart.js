92429b898459f06aeb9fa385bcbad676341f84f3
Nathaniel Bockoven <nbockoven@gmail.com>
2015-12-11 21:44:09 -0800

    sync

--- js/google-bar-chart.js
+++ js/google-bar-chart.js
@@ -7,49 +7,52 @@ var data = [];
 
 var options = {
   animation: {
-    duration: 10000,
-    startup: true
+    duration: 1000,
+    easing: 'out',
+    startup: false
   },
   backgroundColor: 'transparent',
   bars: 'horizontal', // Required for Material Bar Charts.
   colors: ['#006699'],
   hAxis: {
-    // format: 'percent'
-  },
+    // format: 'percent',
+    minValue:0, maxValue:100
+  }
 };
 
 var skillset = [
   [ // on load
-    ['Skill',      'Percent'],
+    ['Skill',      'Proficiency'],
     ['PHP',        0],
     ['MySQL',      0],
     ['JavaScript', 0],
     ['jQuery',     0]
   ],
   [ // on change
-    ['Skill',      'Percent'],
+    ['Skill',      'Proficiency'],
     ['PHP',        80],
-    ['MySQL',      85],
+    ['MySQL',      100],
     ['JavaScript', 60],
     ['jQuery',     70]
   ]
 ];
 
-var chart;
+var THECHART;
 
-function drawChart( iteration ){
+function drawChart(){
   var deferred = $.Deferred();
-  iteration = ( parseInt( iteration ) ) ? 1 : 0;
-  console.log( iteration );
-  google.visualization.events.addListener(chart, 'ready', function(){});
-  // chart.draw(data, options);
-  chart.draw(data[iteration], google.charts.Bar.convertOptions( options ));
+  if( data[CHART_HAS_CHANGED] ){
+    console.log( "showing iteration = "+CHART_HAS_CHANGED );
+    // THECHART.draw(data[CHART_HAS_CHANGED], options);
+    THECHART.draw(data[0], google.charts.Bar.convertOptions( options ));
+  }
+
   return deferred.resolve().promise();
 }
 
 function initGoogleChart(){
   var deferred = $.Deferred();
-  chart = new google.charts.Bar(document.getElementById( chartID ));
+  THECHART = new google.charts.Bar(document.getElementById( chartID ));
   $.each(skillset, function( i, v ){
     data[i] = google.visualization.arrayToDataTable( v );
   });
@@ -58,7 +61,6 @@ function initGoogleChart(){
 
 
 $(document).ready(function(){
-
   $.when( initGoogleChart() )
    .then( drawChart() )
    .then( checkIfChartIsSeen() );
@@ -69,10 +71,10 @@ $(window).scroll( checkIfChartIsSeen );
 function checkIfChartIsSeen(){
   if( !CHART_HAS_CHANGED ){
     // get chart div
-    var chart = $('#'+chartID);
+    var okay = $('#'+chartID);
     // chart lives between...
-    var chartTop    = chart.offset().top;
-    var chartBottom = chartTop + chart.innerHeight;
+    var chartTop    = okay.offset().top;
+    var chartBottom = chartTop + okay.innerHeight;
     // window is showing...
     var windowTop    = window.pageYOffset;
     var windowBottom = windowTop + window.innerHeight;
@@ -83,7 +85,8 @@ function checkIfChartIsSeen(){
     ){
       // change the chart values to trigger chart animation
       CHART_HAS_CHANGED = 1;
-      drawChart( 1 );
+      data[0].setValue(0, 1, skillset[1][1][1]);
+      drawChart();
     }
   }
 }
